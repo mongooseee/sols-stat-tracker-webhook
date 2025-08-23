a22be03d2850b43b65a418ad656ee0f8c549f2b2
Mongoose <mongoose_cluster@outlook.com>
2025-08-23 22:23:28 +0100

    Updated to the Sol's Stat Tracker V2 API

---
+++ config.js
@@ -0,0 +1,31 @@
+const config = {
+    // AUTHENTICATION (REQUIRED)
+    "token": "PLACE YOUR API TOKEN HERE",
+    "webhookURL": "PLACE THE WEBHOOK URL HERE",
+
+    // WEBHOOK USER (OPTIONAL)
+    "overrideUsername": null,
+    "overrideAvatarURL": null,
+
+    "colors": {
+        "success": "#6ab183",
+        "error": "#d85a4b",
+        "none": "#777f8d"
+    },
+
+    "emojis": {
+        "success": "<:green_tick:1365702693326422026>",
+        "error": "<:red_tick:1365702694727188491>",
+        "none": "<:gray_tick:1365702690985738390>"
+    },
+
+    // ADVANCED CONFIGURATION (OPTIONAL) - DO NOT CHANGE THESE CONFIGURATIONS UNLESS YOU KNOW WHAT YOU'RE DOING
+    "gatewayURL": "wss://api.mongoosee.com/solsstattracker/v2/gateway",
+
+    "maxReconnectInterval": 120000,
+    "reconnectOnDuplicateConnection": false, // If true, forces a reconnect attempt on a duplicate connection error, this may cause a reconnection loop.
+    
+    "verboseLogging": true, // If true, logs automated events like connection status, user events are logged regardless of this setting.
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 // Sol's Stat Tracker Webhook Client
 // Created by @mongoo.se
-// Last edited 06/05/2025
+// Last edited 23/08/2025
 
 const WebSocket = require('ws');
 const { WebhookClient, Events, EmbedBuilder } = require('discord.js');
 
-const { token, webhookURL, gatewayURL, maxReconnectInterval, overrideUsername, overrideAvatarURL, colors, emojis } = require('./config.json');
+const {
+    token, webhookURL,                                                               // AUTHENTICATION
+    overrideUsername, overrideAvatarURL, colors, emojis,                             // WEBHOOK USER
+    gatewayURL, maxReconnectInterval, reconnectOnDuplicateConnection, verboseLogging // ADVANCED CONFIGURATION
+} = require('./config');
 
-let reconnectInterval = 5_000;
+let reconnectInterval = 31_000;
 
 const webhookClient = new WebhookClient({ // Use the webhook URL to login to the webhook client
     url: webhookURL
 });
 
 webhookClient.on(Events.Error, (error) => {
-    console.error(`Webhook client error: ${error.message}`)
+    console.error(`ID: ${webhookClient.id} | Webhook client error: ${error.message}`)
 });
 
 const connect = () => {
-    const ws = new WebSocket(gatewayURL, { // Create a WS connetion
+    const ws = new WebSocket(gatewayURL, { // Create a WS connection
         headers: {
             token: token
         }
     });
 
     ws.on('open', () => {
-        console.log(`WS client connected: ${gatewayURL}`);
-        reconnectInterval = 5_000;
+        console.log(`ID: ${webhookClient.id} | WS client connected: ${gatewayURL}`);
+        reconnectInterval = 31_000;
 
         setTimeout(() => {
             if (ws.readyState === ws.OPEN) {
                 const connectedEmbed = new EmbedBuilder()
                     .setDescription(`${emojis.success} **Sol's Stat Tracker** - Connected`)
                     .setColor(colors.success);
-    
-                webhookClient.send({ embeds: [ connectedEmbed ] });
+
+                if (verboseLogging) webhookClient.send({ embeds: [connectedEmbed] });
             }
         }, 1_000);
     });
@@ -49,81 +53,106 @@ const connect = () => {
                         .setDescription(`${emojis.success} **Sol's Stat Tracker** - Enabled`)
                         .setColor(colors.success);
 
-                    webhookClient.send({ embeds: [ enabledEmbed ] });
+                    webhookClient.send({ embeds: [enabledEmbed] });
                     break;
                 case 'disabled':
                     const disabledEmbed = new EmbedBuilder()
                         .setDescription(`${emojis.error} **Sol's Stat Tracker** - Disabled`)
                         .setColor(colors.error);
 
-                    webhookClient.send({ embeds: [ disabledEmbed ] });
+                    webhookClient.send({ embeds: [disabledEmbed] });
                     break;
                 case 'executeWebhook':
                     rawData.data.username = overrideUsername ?? rawData.data.username; // Set overrides
                     rawData.data.avatarURL = overrideAvatarURL ?? rawData.data.avatarURL;
-                    rawData.allowedMentions = { parse: [] }; // Under no circumstances should the webhook be able to send mentions clea
+                    rawData.allowedMentions = { parse: [] }; // Under no circumstances should the webhook be able to send mentions
 
                     webhookClient.send(rawData.data); // Send the message payload
                     break;
                 default:
-                    console.error(`WS client invalid action: ${rawData.action}`);
+                    console.error(`ID: ${webhookClient.id} | WS client invalid action: ${rawData.action}`);
                     break;
             }
         } catch (error) {
-            console.error(`WS client message error: ${error.message}`);
+            console.error(`ID: ${webhookClient.id} | WS client message error: ${error.message}`);
         };
     });
 
     ws.on('close', async (code, reason) => {
-        console.warn(`WS client disconnected: Code ${code}${reason ? ` - ${reason}` : ''}`);
         reason = reason.toString('utf8');
+        console.warn(`ID: ${webhookClient.id} | WS client disconnected: Code ${code}${reason ? ` - ${reason}` : ''}`);
 
         switch (code) {
-            case 1008:
-                if (reason === 'Unauthorized') {
-                    const unauthorizedEmbed = new EmbedBuilder()
-                    .setTitle(`${emojis.error} Unauthorized`)
-                        .setDescription(`The API token is invalid.`)
+            case 4001:
+                if (verboseLogging) {
+                    console.error('The API token is missing.');
+
+                    const missingTokenEmbed = new EmbedBuilder()
+                        .setDescription(`${emojis.error} **Sol's Stat Tracker** - The API token is missing.`)
                         .setColor(colors.error);
 
-                    await webhookClient.send({ embeds: [ unauthorizedEmbed ] });
-                } else if (reason === 'Duplicate connection') {
-                    const unauthorizedEmbed = new EmbedBuilder()
-                        .setTitle(`${emojis.error} Duplicate Connection`)
-                            .setDescription(`The API token is already in-use.`)
-                            .setColor(colors.error);
-
-                    await webhookClient.send({ embeds: [ unauthorizedEmbed ] });
+                    await webhookClient.send({ embeds: [missingTokenEmbed] });
                 }
 
-                break;
-                
-            case 1000:
-                const deletedEmbed = new EmbedBuilder()
-                .setTitle(`${emojis.error} Deleted`)
-                    .setDescription(`The API token has been deleted.`)
-                    .setColor(colors.error);
+                return;
 
-            await webhookClient.send({ embeds: [ deletedEmbed ] });
-            break;
+            case 4002:
+                if (verboseLogging) {
+                    console.error('The API token is invalid.');
+
+                    const invalidTokenEmbed = new EmbedBuilder()
+                        .setDescription(`${emojis.error} **Sol's Stat Tracker** - The API token is invalid.`)
+                        .setColor(colors.error);
+
+                    await webhookClient.send({ embeds: [invalidTokenEmbed] });
+                }
+
+                return;
+
+            case 4004:
+                if (verboseLogging) {
+                    console.error('The API token has been deleted.');
+
+                    const deletedEmbed = new EmbedBuilder()
+                        .setDescription(`${emojis.error} **Sol's Stat Tracker** - The API token has been deleted.`)
+                        .setColor(colors.error);
+
+                    await webhookClient.send({ embeds: [deletedEmbed] });
+                }
+
+                return;
+
+            case 4003:
+                if (verboseLogging) {
+                    console.error('The API token is already in-use.');
+
+                    const duplicateConnectionEmbed = new EmbedBuilder()
+                        .setDescription(`${emojis.error} **Sol's Stat Tracker** - The API token is already in-use.`)
+                        .setColor(colors.error);
+
+                    await webhookClient.send({ embeds: [duplicateConnectionEmbed] });
+                };
+
+                if (!reconnectOnDuplicateConnection) return;
 
             default:
-                console.log(`Reconnecting WS client in ${reconnectInterval}ms...`);
+                console.warn(`ID: ${webhookClient.id} | Reconnecting WS client in ${reconnectInterval}ms...`);
+
+                if (verboseLogging) {
+                    const reconnectingEmbed = new EmbedBuilder()
+                        .setDescription(`${emojis.none} **Sol's Stat Tracker** - Reconnecting`)
+                        .setColor(colors.none);
+
+                    await webhookClient.send({ embeds: [reconnectingEmbed] })
+                };
+
                 setTimeout(connect, reconnectInterval);
-
-                const reconnectingEmbed = new EmbedBuilder()
-                    .setDescription(`${emojis.none} **Sol's Stat Tracker** - Reconnecting`)
-                    .setColor(colors.none);
-        
-                await webhookClient.send({ embeds: [ reconnectingEmbed ] });
-
                 reconnectInterval = Math.min(maxReconnectInterval, reconnectInterval * 2);
-                break;
         }
     });
 
     ws.on('error', async (error) => {
-        console.error(`WS client error: ${error.message}`);
+        console.error(`ID: ${webhookClient.id} | WS client error: ${error.message}`);
         ws.terminate();
     });
 };
